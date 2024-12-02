49375874b33ee2dde8c0b68be08dcbbe3590364a
JEONGGON CHO <wjdrhs62@gmail.com>
2024-12-02 20:45:53 +0900

    refactor: MapModal 수정

--- src/components/account/ProfileCard/index.tsx
+++ src/components/account/ProfileCard/index.tsx
@@ -1,4 +1,4 @@
-import {Card} from "@components/common/Card";
+import Card from "@components/common/Card";
 import Flex from "@components/common/Flex";
 import CardLoading from "@components/skeleton/CardLoading";
 import ProfileImage from "@components/common/ProfileImage";
@@ -19,36 +19,52 @@ const ProfileCard = ({isLoading}: IProfileCardProps) => {
     const {lang} = useThemeStore();
 
     return (
-        <Card padding={"4px 24px 24px"} bgColor={true} borderRadius={0}>
+        <Card
+            padding={"4px 24px 24px"}
+            borderRadius={0}
+        >
             <Flex direction={"column"} gap={"8"}>
-                <Card.Header>
-                    <Flex direction={"row"} align={"center"} gap={12} style={{marginBottom: "16px", width: "100%"}}>
-                        <ProfileImage size={48}/>
-                        <Flex direction={"column"} gap={8}>
-                            <ProfileNameWrapper>{userInfo?.username}</ProfileNameWrapper>
-                            <EmailWrapper>{userInfo?.email.split("@")[0]}</EmailWrapper>
-                        </Flex>
-                    </Flex>
-                </Card.Header>
-
-                <Card.Footer>
+                <Flex direction={"row"} align={"center"} gap={12} style={{marginBottom: "16px", width: "100%"}}>
+                    <ProfileImage size={48}/>
                     <Flex direction={"column"} gap={8}>
-                        {userData?.role &&
-                          <Flex direction={"row"} align={"center"} gap={12}>
-                              {isLoading ?
-                                  <>
-                                      <CardLoading bgColor={"dark"} widthValue={"60px"} heightValue={"20px"}/>
-                                      <CardLoading bgColor={"dark"} widthValue={"150px"} heightValue={"20px"}/>
-                                  </>
-                                  :
-                                  <>
-                                      <TableLabelWrapper lang={lang}>{cardCategories.role[lang]}</TableLabelWrapper>
-                                      <TableContentWrapper>{cardCategories[userData?.role][lang]}</TableContentWrapper>
-                                  </>
-                              }
-                          </Flex>
-                        }
+                        <ProfileNameWrapper>{userInfo?.username}</ProfileNameWrapper>
+                        <EmailWrapper>{userInfo?.email.split("@")[0]}</EmailWrapper>
+                    </Flex>
+                </Flex>
 
+                <Flex direction={"column"} gap={8}>
+                    {userData?.role &&
+                      <Flex direction={"row"} align={"center"} gap={12}>
+                          {isLoading ?
+                              <>
+                                  <CardLoading bgColor={"dark"} widthValue={"60px"} heightValue={"20px"}/>
+                                  <CardLoading bgColor={"dark"} widthValue={"150px"} heightValue={"20px"}/>
+                              </>
+                              :
+                              <>
+                                  <TableLabelWrapper lang={lang}>{cardCategories.role[lang]}</TableLabelWrapper>
+                                  <TableContentWrapper>{cardCategories[userData?.role][lang]}</TableContentWrapper>
+                              </>
+                          }
+                      </Flex>
+                    }
+
+                    <Flex direction={"row"} align={"center"} gap={12}>
+                        {isLoading ?
+                            <>
+                                <CardLoading bgColor={"dark"} widthValue={"60px"} heightValue={"20px"}/>
+                                <CardLoading bgColor={"dark"} widthValue={"150px"} heightValue={"20px"}/>
+                            </>
+                            :
+                            <>
+                                <TableLabelWrapper lang={lang}>{cardCategories.studentId[lang]}</TableLabelWrapper>
+                                <TableContentWrapper>{userInfo?.studentId}</TableContentWrapper>
+                            </>
+                        }
+                    </Flex>
+
+                    {userData?.role === "student" &&
+                      <>
                         <Flex direction={"row"} align={"center"} gap={12}>
                             {isLoading ?
                                 <>
@@ -57,60 +73,43 @@ const ProfileCard = ({isLoading}: IProfileCardProps) => {
                                 </>
                                 :
                                 <>
-                                    <TableLabelWrapper lang={lang}>{cardCategories.studentId[lang]}</TableLabelWrapper>
-                                    <TableContentWrapper>{userInfo?.studentId}</TableContentWrapper>
+                                    <TableLabelWrapper lang={lang}>{cardCategories.year[lang]}</TableLabelWrapper>
+                                    <TableContentWrapper>{userData?.year}</TableContentWrapper>
                                 </>
                             }
                         </Flex>
+                        <Flex direction={"row"} align={"center"} gap={12}>
+                            {isLoading ?
+                                <>
+                                    <CardLoading bgColor={"dark"} widthValue={"60px"} heightValue={"20px"}/>
+                                    <CardLoading bgColor={"dark"} widthValue={"150px"} heightValue={"20px"}/>
+                                </>
+                                :
+                                <>
+                                    <TableLabelWrapper lang={lang}>{cardCategories.studio[lang]}</TableLabelWrapper>
+                                    <TableContentWrapper>{userData?.studio} {cardCategories.professor[lang]}</TableContentWrapper>
+                                </>
+                            }
+                        </Flex>
+                      </>
+                    }
 
-                        {userData?.role === "student" &&
-                          <>
-                            <Flex direction={"row"} align={"center"} gap={12}>
-                                {isLoading ?
-                                    <>
-                                        <CardLoading bgColor={"dark"} widthValue={"60px"} heightValue={"20px"}/>
-                                        <CardLoading bgColor={"dark"} widthValue={"150px"} heightValue={"20px"}/>
-                                    </>
-                                    :
-                                    <>
-                                        <TableLabelWrapper lang={lang}>{cardCategories.year[lang]}</TableLabelWrapper>
-                                        <TableContentWrapper>{userData?.year}</TableContentWrapper>
-                                    </>
-                                }
-                            </Flex>
-                            <Flex direction={"row"} align={"center"} gap={12}>
-                                {isLoading ?
-                                    <>
-                                        <CardLoading bgColor={"dark"} widthValue={"60px"} heightValue={"20px"}/>
-                                        <CardLoading bgColor={"dark"} widthValue={"150px"} heightValue={"20px"}/>
-                                    </>
-                                    :
-                                    <>
-                                        <TableLabelWrapper lang={lang}>{cardCategories.studio[lang]}</TableLabelWrapper>
-                                        <TableContentWrapper>{userData?.studio} {cardCategories.professor[lang]}</TableContentWrapper>
-                                    </>
-                                }
-                            </Flex>
-                          </>
-                        }
-
-                        {userData?.role === "assistant" &&
-                          <Flex direction={"row"} align={"center"} gap={12}>
-                              {isLoading ?
-                                  <>
-                                      <CardLoading bgColor={"dark"} widthValue={"60px"} heightValue={"20px"}/>
-                                      <CardLoading bgColor={"dark"} widthValue={"150px"} heightValue={"20px"}/>
-                                  </>
-                                  :
-                                  <>
-                                      <TableLabelWrapper lang={lang}>{cardCategories.lab[lang]}</TableLabelWrapper>
-                                      <LabNameWrapper lab={!!userData.lab}>{userData?.lab || `${cardCategories.emptyLab[lang]}`}</LabNameWrapper>
-                                  </>
-                              }
-                          </Flex>
-                        }
-                    </Flex>
-                </Card.Footer>
+                    {userData?.role === "assistant" &&
+                      <Flex direction={"row"} align={"center"} gap={12}>
+                          {isLoading ?
+                              <>
+                                  <CardLoading bgColor={"dark"} widthValue={"60px"} heightValue={"20px"}/>
+                                  <CardLoading bgColor={"dark"} widthValue={"150px"} heightValue={"20px"}/>
+                              </>
+                              :
+                              <>
+                                  <TableLabelWrapper lang={lang}>{cardCategories.lab[lang]}</TableLabelWrapper>
+                                  <LabNameWrapper lab={!!userData.lab}>{userData?.lab || `${cardCategories.emptyLab[lang]}`}</LabNameWrapper>
+                              </>
+                          }
+                      </Flex>
+                    }
+                </Flex>
             </Flex>
         </Card>
     );
